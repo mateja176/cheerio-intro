5ce6cf59b4df52bd35b8825bb3fde2e1d3226cde
Mateja Petrovic <mateja176@gmail.com>
2019-02-23 14:40:33 +0100

    Scrapes google cheerio search and stores the first search result in first.html

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("Welcome");
+import * as cheerio from "cheerio"
+import fetch from "node-fetch"
+
+const url = "https://www.google.com/search?q=cheerio"
+
+fetch(url)
+  .then(res => res.text())
+  .then(text => cheerio.load(text))
+  .then($ => $("#ires ol"))
+  .then($ => $.children())
+  .then($ => $.first())
+  .then($ => $.html())
+  .then(console.log)
+  .catch(console.error)
